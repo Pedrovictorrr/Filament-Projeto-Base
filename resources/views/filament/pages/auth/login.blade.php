75155322db221b54139eca1514bcddbbba0f5514
w2o <pedro.fabreu97@gmail.com>
2024-02-13 11:11:43 -0300

    update core project

--- resources/views/filament/pages/auth/login.blade.php
+++ resources/views/filament/pages/auth/login.blade.php
@@ -1,27 +1,33 @@
 <x-filament-panels::page.simple>
+    @if (!$this->ResetPassword)
 
-    @if (filament()->hasRegistration())
-        <x-slot name="subheading">
-            {{ __('filament-panels::pages/auth/login.actions.register.before') }}
+        @if (filament()->hasRegistration())
+            <x-slot name="subheading">
+                {{ __('filament-panels::pages/auth/login.actions.register.before') }}
 
-            {{ $this->registerAction }}
-        </x-slot>
+                {{ $this->registerAction }}
+            </x-slot>
+        @endif
+
+        {{ \Filament\Support\Facades\FilamentView::renderHook('panels::auth.login.form.before') }}
+
+        <x-filament-panels::form wire:submit="authenticate">
+            <style>
+                .fi-body {
+                    background-image: url("/images/background-inicio.jpg") !important;
+                    background-size: cover;
+                    background-position: center center;
+                }
+            </style>
+            {{ $this->form }}
+
+            <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
+        </x-filament-panels::form>
+    @else
+        <x-filament-panels::form wire:submit="PasswordResetRequest">
+            {{ $this->ResetPasswordForm }}
+            <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
+        </x-filament-panels::form>
     @endif
-
-    {{ \Filament\Support\Facades\FilamentView::renderHook('panels::auth.login.form.before') }}
-
-    <x-filament-panels::form wire:submit="authenticate">
-        <style>
-            .fi-body {
-                background-image: url("/images/background-inicio.jpg") !important;
-                background-size: cover;
-                background-position: center center;
-            }
-        </style>
-        {{ $this->form }}
-
-        <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
-    </x-filament-panels::form>
-
     {{ \Filament\Support\Facades\FilamentView::renderHook('panels::auth.login.form.after') }}
 </x-filament-panels::page.simple>
